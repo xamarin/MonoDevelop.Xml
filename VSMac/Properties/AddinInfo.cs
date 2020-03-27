77068b05f48b02b8a3c52e89186000bb2b46f590
Kirill Osenkov <github@osenkov.com>
2020-03-26 18:52:08 -0700

    Expand AddinInfo.cs

--- VSMac/Properties/AddinInfo.cs
+++ VSMac/Properties/AddinInfo.cs
@@ -3,10 +3,16 @@ using Mono.Addins.Description;
 
 [assembly: Addin (
 	"MonoDevelop.Xml",
-	Namespace = "MonoDevelop.Xml"
+	Namespace = "MonoDevelop.Xml",
+	Version = MonoDevelop.BuildInfo.Version,
+	Category = "IDE Extensions"
 )]
 
 [assembly: AddinName ("Xml Editor")]
 [assembly: AddinCategory ("IDE extensions")]
 [assembly: AddinDescription ("Editing support for XML files")]
 [assembly: AddinAuthor ("Mikayla Hutchinson")]
+
+[assembly: AddinDependency ("Core", MonoDevelop.BuildInfo.Version)]
+[assembly: AddinDependency ("Ide", MonoDevelop.BuildInfo.Version)]
+[assembly: AddinDependency ("TextEditor", MonoDevelop.BuildInfo.Version)]
