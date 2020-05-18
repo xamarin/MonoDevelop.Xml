f06670ba820dcf42c857bb58b2f9c9304618f5e2
Kirill Osenkov <github@osenkov.com>
2020-05-17 18:50:05 -0700

    Make sure logging only happens in Debug.

--- Editor/Completion/LoggingService.cs
+++ Editor/Completion/LoggingService.cs
@@ -2,13 +2,17 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
+using System.Diagnostics;
 
 namespace MonoDevelop.Xml.Editor.Completion
 {
 	//FIXME: replace this MD shim with something better
 	internal class LoggingService
 	{
+		[Conditional("DEBUG")]
 		internal static void LogWarning (string formatString, params object[] args) => Console.WriteLine (formatString, args);
+
+		[Conditional("DEBUG")]
 		internal static void LogDebug (string formatString, params object[] args) => Console.WriteLine (formatString, args);
 	}
 }
